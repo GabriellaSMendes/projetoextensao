89ee42c473ddf62c594ba4e8b0e1212bc78f5baa
MatheusSarretto <matheus.sarretto@gmail.com>
2025-11-11 21:23:00 -0300

    Update Backend

--- database/banco.sql
+++ database/banco.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE tropicalmix_db;
+USE tropicalmix_db;
+
 CREATE TABLE IF NOT EXISTS categoria (
     id_categoria INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
@@ -10,9 +13,9 @@ CREATE TABLE IF NOT EXISTS produto (
     sabor VARCHAR(50),
     marca VARCHAR(100),
     quantidade INT DEFAULT 0,
-    data_vencimento DATE DEFAULT CURRENT_DATE,
+    data_vencimento DATE,
     preco_unitario DECIMAL(10,2) NOT NULL,
-    dt_cadastro DATE DEFAULT CURRENT_DATE,
+    dt_cadastro DATE,
     id_categoria INT,
     FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria)
 );
@@ -20,7 +23,7 @@ CREATE TABLE IF NOT EXISTS produto (
 CREATE TABLE IF NOT EXISTS usuario (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nome_usuario VARCHAR(100) NOT NULL,
-    cpf VARCHAR(14)
+    cpf VARCHAR(14),
     email VARCHAR(150) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL,
     nivel_acesso ENUM('admin','vendedor') DEFAULT 'vendedor'
@@ -33,7 +36,7 @@ CREATE TABLE IF NOT EXISTS cliente (
     telefone VARCHAR(20),
     email VARCHAR(150),
     endereco VARCHAR(255),
-    dt_cadastro DATE DEFAULT CURRENT_DATE
+    dt_cadastro DATE
 );
 
 CREATE TABLE IF NOT EXISTS venda (
@@ -45,19 +48,6 @@ CREATE TABLE IF NOT EXISTS venda (
     mtd_pagamento ENUM('dinheiro','cartao','pix','outros'),
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
-);
-
-CREATE TABLE IF NOT EXISTS venda_estoque (
-    id_venda_estoque INT AUTO_INCREMENT PRIMARY KEY,
-    id_venda INT NOT NULL,
-    id_estoque INT NOT NULL,
-    id_produto INT NOT NULL,
-    qtdd_venda INT NOT NULL,
-    preco_unitario DECIMAL(10,2),
-    subtotal DECIMAL(10,2) GENERATED ALWAYS AS (quantidade_venda * preco_unitario) STORED,
-    FOREIGN KEY (id_venda) REFERENCES vendas(id_venda),
-    FOREIGN KEY (id_produto) REFERENCES produto(id_produto),
-    FOREIGN KEY (id_estoque) REFERENCES produto(id_estoque)
 );
 
 CREATE TABLE IF NOT EXISTS estoque (
@@ -86,5 +76,18 @@ CREATE TABLE IF NOT EXISTS abastece (
     qtdd_recebida INT NOT NULL,
     valor_unitario DECIMAL(10,2),
     FOREIGN KEY (id_fornecedor) REFERENCES fornecedor(id_fornecedor),
+    FOREIGN KEY (id_estoque) REFERENCES estoque(id_estoque)
+);
+
+CREATE TABLE IF NOT EXISTS venda_estoque (
+    id_venda_estoque INT AUTO_INCREMENT PRIMARY KEY,
+    id_venda INT NOT NULL,
+    id_estoque INT NOT NULL,
+    id_produto INT NOT NULL,
+    qtdd_venda INT NOT NULL,
+    preco_unitario DECIMAL(10,2),
+    subtotal DECIMAL(10,2) GENERATED ALWAYS AS (qtdd_venda * preco_unitario) STORED, 
+    FOREIGN KEY (id_venda) REFERENCES venda(id_venda),
+    FOREIGN KEY (id_produto) REFERENCES produto(id_produto),
     FOREIGN KEY (id_estoque) REFERENCES estoque(id_estoque)
 );

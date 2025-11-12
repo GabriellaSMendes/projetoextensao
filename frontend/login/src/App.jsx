89bdb7fed2abde4d3d34f2cbd2510b537de8f422
MATHEUS VICTOR <140826710+MatheusFeliciano0904@users.noreply.github.com>
2025-11-11 22:56:06 -0300

    Atualização da Home
    Erro de File

--- frontend/login/src/App.jsx
+++ frontend/login/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";         // ⬅️ nova Home
+import Home from "./pages/Home";        
 import Cadastro from "./pages/Cadastro";
 import Estoque from "./pages/Estoque";
 import Vendas from "./pages/Vendas";
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />        {/* Home simples */}
+        <Route path="/" element={<Home />} />  
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/estoque" element={<Estoque />} />
         <Route path="/vendas" element={<Vendas />} />

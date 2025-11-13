37c5a39157d4e020d9d29837e52c2317e8cf2795
Gabriella <gabriella.smendes@outlook.com>
2025-11-12 21:12:37 -0300

    feat: Implementação da Navbar

--- frontend/src/components/navbar/index.jsx
+++ frontend/src/components/navbar/index.jsx
@@ -3,9 +3,15 @@ import iconLogo from "../../assets/icon-logo.png"
 import "./style.css"
 import { Link } from "react-router-dom"
 import { FaUserCircle } from "react-icons/fa"
+import { useState } from "react"
 
-function NavBar(){
-    return(
+function Navbar() {
+
+    const [menuAberto, setMenuAberto] = useState(false)
+    const toggleMenu = () => setMenuAberto(!menuAberto)
+
+
+    return (
         <header className="navbar">
             <div className="navbar-left">
                 <img src={logo} alt="Tropical Mix" className="navbar-logo" />
@@ -15,11 +21,38 @@ function NavBar(){
                 <Link to="/" className="nav-link">Home</Link>
                 <Link to="/estoque" className="nav-link">Estoque</Link>
                 <Link to="/vendas" className="nav-link">Vendas</Link>
-
-                
-
+                <Link to="/">
+                    <img src={iconLogo} alt="Home" className="navbar-sun" />
+                </Link>
+                <Link to="/relatorio">Relatório</Link>
+                <Link to="/fornecedores">Fornecedores</Link>
             </nav>
+
+
+            {/*Botão de usuário com menu dropdown*/}
+            <div className="navbar-right">
+                <div className="user-menu-container">
+                    <FaUserCircle
+                        className="navbar-user"
+                        onClick={toggleMenu}
+                    />
+
+                    {/* dropdown do usuário */}
+                    {menuAberto && (
+                        <div className="user-dropdown">
+                            <Link to="/perfil" className="dropdown-item">Ver perfil</Link>
+                            <Link to="/cadastro" className="dropdown-item">Cadastrar novo usuário</Link>
+                            <hr className="dropdown-divider" />
+                            <button className="dropdown-item logout-btn">
+                                <FaSignOutAlt className="logout-icon" /> Sair
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </div>
 
         </header>
     );
 }
+
+export default Navbar;

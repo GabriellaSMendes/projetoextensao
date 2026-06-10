7f556fadaee8208406c957ddbbcc0a45ffb1c5b7
Gabriella <gabriella.smendes@outlook.com>
2026-06-09 23:57:18 -0300

    Ajusta URL da API para deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    //baseURL: window.APP_CONFIG.API_URL,
-    baseURL: '/api',
+    baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:5000/api", #localmente usa seu backend local; no deploy, o Render do front vai receber uma variável chamada VITE_API_URL apontando para o backend publicado.
 });
 
 export default api;
